f1189c27e43b9b4ae4bdb7e71ed9f250fc28dd95
DimasArkaan <dimasarkaan11@gmail.com>
2026-02-23 14:02:47 +0700

    Iterasi 2 - Fix Bug 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
         Gate::policy(Sparepart::class, SparepartPolicy::class);
         Gate::policy(\App\Models\User::class, \App\Policies\UserPolicy::class);
 
