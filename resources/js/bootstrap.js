942abe845d9ca125fa3eeaaafb79723ba69b7e11
DimasArkaan <dimasarkaan11@gmail.com>
2026-02-23 14:28:58 +0700

    Iterasi 2 - Fix Bug 5

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,6 +21,8 @@ if (import.meta.env.VITE_PUSHER_APP_KEY) {
         broadcaster: 'pusher',
         key: import.meta.env.VITE_PUSHER_APP_KEY,
         cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-        forceTLS: true
+        forceTLS: true,
+        enabledTransports: ['ws', 'wss'],
+        disableStats: true,
     });
 }
